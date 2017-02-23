f6205ed33d2c8dfbaa2f22756b17721a5bd6b765
Marco Senna Fontanari <marcosennafontanari@gmail.com>
2017-02-23 16:07:19 -0300

    Encapsulamento dos atributos

--- ClassePessoa.java
+++ ClassePessoa.java
@@ -1,9 +1,12 @@
 public class Pessoa{
-	String nome;
+	private String nome;
 	int idade;
 	
 	public Pessoa(String nome, int idade){
 		this.nome=nome;
 		this.idade=idade;
-	}	
+	}		
+	public String getNome(){
+	return nome;
+	}
 }
